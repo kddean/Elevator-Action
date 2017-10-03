d326b82b4fb21abeb9d57fb258c397b8d391e7a2
MightyK1997 <31252492+MightyK1997@users.noreply.github.com>
2017-10-03 11:16:26 -0600

    added start screen animations

--- PhaserTypeScript/PhaserTypeScript/MainMenu.ts
+++ PhaserTypeScript/PhaserTypeScript/MainMenu.ts
@@ -6,7 +6,7 @@
         preload() {
             //this.game.load.image('button', 'assets/start.png');
             this.game.load.audio('start', 'assets/shoot.wav');
-            this.game.load.spritesheet('pressStart', 'assets/r_introscreen_space.png', 1080, 1920);
+            this.game.load.spritesheet('pressStart', 'assets/r_introscreen_space.png', 1920, 1080);
             this.fireButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
         }
@@ -14,8 +14,8 @@
             //var button = this.game.add.button(this.game.world.centerX - 100, this.game.world.centerY, 'button', this.actionOnClick, this, 2, 1, 0);
             //button.scale.setTo(0.5, 0.5);
             this.startScreen = this.game.add.sprite(0, 0, 'pressStart');
-            //this.game.physics.arcade.enable(this.startScreen);
-            //this.startScreen.animations.add('idle', [1, 2]);
+            this.game.physics.arcade.enable(this.startScreen);
+            this.startScreen.animations.add('idle', [0, 1], 0 , true);
             this.music = this.add.audio('shoot');
             
             
@@ -25,8 +25,8 @@
             if (this.fireButton.isDown) {
                 this.state.start('Game', true, false);
             }
-            //this.startScreen.animations.play('idle');
-            //this.startScreen.animations.currentAnim.speed = 10;
+            this.startScreen.animations.play('idle');
+            this.startScreen.animations.currentAnim.speed = 10;
         }
         //actionOnClick() {
             
