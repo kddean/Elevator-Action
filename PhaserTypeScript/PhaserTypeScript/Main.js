942e1e5eb3b3dff7b77be71fe674a186f2c3a4b9
MightyK1997 <31252492+MightyK1997@users.noreply.github.com>
2017-10-03 11:05:59 -0600

    added some start screen things.

--- PhaserTypeScript/PhaserTypeScript/Main.js
+++ PhaserTypeScript/PhaserTypeScript/Main.js
@@ -873,14 +873,25 @@ var ElevatorAction;
             return _super !== null && _super.apply(this, arguments) || this;
         }
         MainMenu.prototype.preload = function () {
-            this.game.load.image('button', 'assets/start.png');
+            //this.game.load.image('button', 'assets/start.png');
+            this.game.load.audio('start', 'assets/shoot.wav');
+            this.game.load.spritesheet('pressStart', 'assets/r_introscreen_space.png', 1080, 1920);
+            this.fireButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         };
         MainMenu.prototype.create = function () {
-            var button = this.game.add.button(this.game.world.centerX - 100, this.game.world.centerY, 'button', this.actionOnClick, this, 2, 1, 0);
-            button.scale.setTo(0.5, 0.5);
+            //var button = this.game.add.button(this.game.world.centerX - 100, this.game.world.centerY, 'button', this.actionOnClick, this, 2, 1, 0);
+            //button.scale.setTo(0.5, 0.5);
+            this.startScreen = this.game.add.sprite(0, 0, 'pressStart');
+            //this.game.physics.arcade.enable(this.startScreen);
+            //this.startScreen.animations.add('idle', [1, 2]);
+            this.music = this.add.audio('shoot');
         };
-        MainMenu.prototype.actionOnClick = function () {
-            this.game.state.start('Game', true, false);
+        MainMenu.prototype.update = function () {
+            if (this.fireButton.isDown) {
+                this.state.start('Game', true, false);
+            }
+            //this.startScreen.animations.play('idle');
+            //this.startScreen.animations.currentAnim.speed = 10;
         };
         return MainMenu;
     }(Phaser.State));
--- PhaserTypeScript/PhaserTypeScript/MainMenu.ts
+++ PhaserTypeScript/PhaserTypeScript/MainMenu.ts
@@ -1,14 +1,35 @@
 ﻿module ElevatorAction {
     export class MainMenu extends Phaser.State {
+        music: Phaser.Sound;
+        startScreen: Phaser.Sprite;
+        fireButton: Phaser.Key;
         preload() {
-            this.game.load.image('button', 'assets/start.png');
+            //this.game.load.image('button', 'assets/start.png');
+            this.game.load.audio('start', 'assets/shoot.wav');
+            this.game.load.spritesheet('pressStart', 'assets/r_introscreen_space.png', 1080, 1920);
+            this.fireButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
         }
         create() {
-            var button = this.game.add.button(this.game.world.centerX - 100, this.game.world.centerY, 'button', this.actionOnClick, this, 2, 1, 0);
-            button.scale.setTo(0.5, 0.5);
+            //var button = this.game.add.button(this.game.world.centerX - 100, this.game.world.centerY, 'button', this.actionOnClick, this, 2, 1, 0);
+            //button.scale.setTo(0.5, 0.5);
+            this.startScreen = this.game.add.sprite(0, 0, 'pressStart');
+            //this.game.physics.arcade.enable(this.startScreen);
+            //this.startScreen.animations.add('idle', [1, 2]);
+            this.music = this.add.audio('shoot');
+            
+            
+            
         }
-        actionOnClick() {
-            this.game.state.start('Game', true, false);
+        update() {
+            if (this.fireButton.isDown) {
+                this.state.start('Game', true, false);
+            }
+            //this.startScreen.animations.play('idle');
+            //this.startScreen.animations.currentAnim.speed = 10;
         }
+        //actionOnClick() {
+            
+        //}
     }
 }
